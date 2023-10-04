618d8a54585b8ba1c973a29319b280f773570776
Khalid Ahmad <khalid31894@gmail.com>
2023-10-04 09:58:16 +0500

    Build V3.0 VC3

--- Assets/4_MyUtility/Ad Managers/ScreenTouch_Ads.cs
+++ Assets/4_MyUtility/Ad Managers/ScreenTouch_Ads.cs
@@ -1,43 +1,43 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using DG.Tweening;
-//using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
-//public class ScreenTouch_Ads : MonoBehaviour
-//{
-//    public static ScreenTouch_Ads instance;
-//    public float Delay_Time;
-//    public float get_touch = 0;
-//    public bool IsPlay_Count;
-//    private void Awake()
-//    {
-//        print("in timer");
-//        instance = this;
-//        Input.multiTouchEnabled = false;
-//        IsPlay_Count = true;
-//        //Delay_Time=20;
-//       // AssignAdIds_CB.instance.play_Ad = false;
-//    }
-//    void Update()
-//    {
-//        if (IsPlay_Count && PlayerPrefs.GetInt("unlockall") == 0)
-//        {
-//            get_touch += Time.deltaTime;
-//            if (get_touch >= Delay_Time)
-//            {
-//                AssignAdIds_CB.instance.play_Ad = true;
-//                IsPlay_Count = false;
-//            }
-//        }
-//        else if (IsPlay_Count && PlayerPrefs.GetInt("UnlockGame") == 1)
-//        {
-//            get_touch += Time.deltaTime;
-//            if (get_touch >= Delay_Time)
-//            {
-//                AssignAdIds_CB.instance.play_Ad = true;
-//                IsPlay_Count = false;
-//            }
-//        }
-//    }
-//}
+public class ScreenTouch_Ads : MonoBehaviour
+{
+    public static ScreenTouch_Ads instance;
+    public float Delay_Time;
+    public float get_touch = 0;
+    public bool IsPlay_Count;
+    private void Awake()
+    {
+        print("in timer");
+        instance = this;
+        Input.multiTouchEnabled = false;
+        IsPlay_Count = true;
+        //Delay_Time=20;
+        // AssignAdIds_CB.instance.play_Ad = false;
+    }
+    void Update()
+    {
+        if (IsPlay_Count && PlayerPrefs.GetInt("unlockall") == 0)
+        {
+            get_touch += Time.deltaTime;
+            if (get_touch >= Delay_Time)
+            {
+                AssignAdIds_CB.instance.play_Ad = true;
+                IsPlay_Count = false;
+            }
+        }
+        else if (IsPlay_Count && PlayerPrefs.GetInt("UnlockGame") == 1)
+        {
+            get_touch += Time.deltaTime;
+            if (get_touch >= Delay_Time)
+            {
+                AssignAdIds_CB.instance.play_Ad = true;
+                IsPlay_Count = false;
+            }
+        }
+    }
+}
